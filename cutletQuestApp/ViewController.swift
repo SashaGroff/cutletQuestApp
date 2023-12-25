0f8c9d613d043cac2e7afecf4ccd812156d4570d
SashaGroff <goncharov.der@gmail.com>
2023-12-25 20:33:45 +0300

    Initial Commit

---
+++ cutletQuestApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  cutletQuestApp
+//
+//  Created by Aleksandr Goncharov on 25.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
