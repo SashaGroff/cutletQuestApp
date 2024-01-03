8ba4e30406f4e2e47c5b9cca8104b586ef98dfd9
RCherezbiev <149434791+RCherezbiev@users.noreply.github.com>
2024-01-04 02:02:35 +0300

    Add registration ViewController

---
+++ cutletQuestApp/ViewControllers/RegistrationViewController.swift
@@ -0,0 +1,36 @@
+//
+//  RegistrationViewController.swift
+//  cutletQuestApp
+//
+//  Created by Rustam Cherezbiev on 03.01.2024.
+//
+
+import UIKit
+
+class RegistrationViewController: UIViewController {
+    // MARK: - IB Outlets
+    
+    // MARK: - Public Properties
+    
+    // MARK: - Private Properties
+    
+    // MARK: - Initializers
+    
+    // MARK: - Overrides Methods
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+    // MARK: - IB Actions
+    @IBAction func registrationAction() {
+        dismiss(animated: true)
+    }
+    
+    // MARK: - Public Methods
+    
+    // MARK: - Private Methods
+
+}
