1249908b73c0b0a28ce2b760b3c8e35a3dce3802
RCherezbiev <149434791+RCherezbiev@users.noreply.github.com>
2024-01-04 02:01:29 +0300

    Add model user with userdata

---
+++ cutletQuestApp/Models/User.swift
@@ -0,0 +1,12 @@
+//
+//  User.swift
+//  cutletQuestApp
+//
+//  Created by Rustam Cherezbiev on 04.01.2024.
+//
+
+struct User {
+    let userName = "Рустам"
+    let userLogin = "123123"
+    let userPassword = "123123"
+}
