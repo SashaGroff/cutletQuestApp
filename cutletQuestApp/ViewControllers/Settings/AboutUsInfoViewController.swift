64013dce743acb3397e498a400b0669b949fbac1
NatashaOvdina <natashaovdina@yahoo.com>
2024-01-18 15:56:11 +0300

    Created storyboard, began to write a code

---
+++ cutletQuestApp/ViewControllers/Settings/AboutUsInfoViewController.swift
@@ -0,0 +1,32 @@
+//
+//  AboutUsInfoViewController.swift
+//  cutletQuestApp
+//
+//  Created by Natalia Ovdina on 18.01.2024.
+//
+
+import UIKit
+
+final class AboutUsInfoViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.rowHeight = 90
+    }
+    
+
+    // MARK: - Navigation
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+    }
+
+}
+
+// MARK: - UITableViewDataSource
+
+extension AboutUsInfoViewController {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+}
